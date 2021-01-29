a6e4017a27a354d145a584a26a7d24639e78bf70
benevpi <31006592+benevpi@users.noreply.github.com>
2021-01-29 17:30:41 +0000

    added high and fixed a bunch of terrible errors

--- DebouncerPIO.py
+++ DebouncerPIO.py
@@ -16,11 +16,39 @@ def debounce_low():
     wait(1, pin, 0)
     label("skip")
     
+@rp2.asm_pio()
+def debounce_high():
+    wait(1, pin, 0)
+    set(y,7)
+    label("loop")
+    nop() [10]
+    jmp(y_dec, "loop")
+    in_(pins,1)
+    mov(isr, y)
+    jmp(not_y, "skip")
+    irq(block, rel(0))
+    wait(0, pin, 0)
+    label("skip")
+
 class DebouncerLowPIO:
     
     def __init__(self, statemachine, pin_number, handler):
-        self.pin = Pin(0, Pin.IN, Pin.PULL_UP)
-        self.sm  = rp2.StateMachine(0, debounce_low, in_base=self.pin, freq=2000)
+        self.pin = Pin(pin_number, Pin.IN, Pin.PULL_UP)
+        self.sm  = rp2.StateMachine(statemachine, debounce_low, in_base=self.pin, freq=2000)
+        self.sm.irq(handler)
+        self.sm.active(1)
+        
+    def stop(self):
+        self.sm.active(0)
+        
+    def start(self):
+        self.sm.active(1)
+        
+class DebouncerHighPIO:
+    
+    def __init__(self, statemachine, pin_number, handler):
+        self.pin = Pin(pin_number, Pin.IN, Pin.PULL_DOWN)
+        self.sm  = rp2.StateMachine(statemachine, debounce_high, in_base=self.pin, freq=2000)
         self.sm.irq(handler)
         self.sm.active(1)
         
--- debouncer_test.py
+++ debouncer_test.py
@@ -1,6 +1,11 @@
 import DebouncerPIO
 
 def handler(sm):
-    print("Here")
+    print("Here low")
+    
+def handler_high(sm):
+    print("Here high")
     
 button = DebouncerPIO.DebouncerLowPIO(0,0,handler)
+
+button2 = DebouncerPIO.DebouncerHighPIO(1,1,handler_high)
