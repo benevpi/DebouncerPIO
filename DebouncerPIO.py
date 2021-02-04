b6d060dd1837ad807f3aa35e2f552713c0fb6c2f
benevpi <31006592+benevpi@users.noreply.github.com>
2021-02-04 17:16:52 +0000

    more robustness and I think I'm side-stepping a bug in pio

--- DebouncerPIO.py
+++ DebouncerPIO.py
@@ -5,30 +5,75 @@ import time
 @rp2.asm_pio()
 def debounce_low():
     wait(0, pin, 0)
+    
     set(y,7)
     label("loop")
-    nop() [10]
-    jmp(y_dec, "loop")
-    in_(pins,1)
+    jmp(y_dec, "loop") [10]
+    in_(pins,1) 
     mov(isr, y)
     jmp(y, "skip")
+    
+    set(y,7)
+    label("loop2")
+    jmp(y_dec, "loop2") [10]
+    in_(pins,1) 
+    mov(isr, y)
+    jmp(y, "skip")
+    
+    set(y,7)
+    label("loop3")
+    jmp(y_dec, "loop3") [10]
+    in_(pins,1) 
+    mov(isr, y)
+    jmp(y, "skip")
+    
     irq(block, rel(0))
     wait(1, pin, 0)
-    label("skip")
+    label("skip") #no idea why this isn't triggering the not-wrapping bug that debounce-high is
     
 @rp2.asm_pio()
 def debounce_high():
     wait(1, pin, 0)
+    
     set(y,7)
-    label("loop")
-    nop() [10]
-    jmp(y_dec, "loop")
+    
+    label("loophigh")
+    jmp(y_dec, "loophigh") [10]
+    
     in_(pins,1)
-    mov(isr, y)
-    jmp(not_y, "skip")
+    mov(y, isr)
+    jmp(not_y, "skiphigh")
+    
+    #let's do as many checks as we can
+    #there's not an obvious way of putting this in a loop, 
+    #but there is a spare scratch register, so perhaps there's an option
+    
+    set(y,7)
+    
+    label("loophigh2")
+    jmp(y_dec, "loophigh2") [10]
+    
+    in_(pins,1)
+    mov(y, isr)
+    jmp(not_y, "skiphigh")
+    
+    #and once more
+    
+    set(y,7)
+    
+    label("loophigh3")
+    jmp(y_dec, "loophigh3") [10]
+    
+    in_(pins,1)
+    mov(y, isr)
+    jmp(not_y, "skiphigh")
+    
+    
     irq(block, rel(0))
+    
     wait(0, pin, 0)
-    label("skip")
+    label("skiphigh")
+    set(y,1) # just a test -- doesn't work if removed. looks like it's not wrapping
 
 class DebouncerLowPIO:
     
@@ -48,7 +93,7 @@ class DebouncerHighPIO:
     
     def __init__(self, statemachine, pin_number, handler):
         self.pin = Pin(pin_number, Pin.IN, Pin.PULL_DOWN)
-        self.sm  = rp2.StateMachine(statemachine, debounce_high, in_base=self.pin, freq=2000)
+        self.sm  = rp2.StateMachine(statemachine, debounce_high, in_base=self.pin, freq=3000)
         self.sm.irq(handler)
         self.sm.active(1)
         
